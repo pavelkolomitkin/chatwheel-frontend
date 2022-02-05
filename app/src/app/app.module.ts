2dabadca93f476cf0ce444bc8b1c65dd9766fea0
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2022-02-05 21:44:30 +0300

    add localize and ng-bootstrap

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CoreModule} from "./core/core.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {CoreModule} from "./core/core.module";
   imports: [
     CoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
