e0c4d44c5eb40fecfb7c159f415b6d8c87b63d93
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2022-04-04 05:04:54 +0300

    added styling and dynaic to the langind page

--- app/src/app/security/components/layout/layout.component.ts
+++ app/src/app/security/components/layout/layout.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {UserMediaService} from "../../../core/services/user-media.service";
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, AfterContentInit, OnDestroy {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  @ViewChild('videoElement') videoElement: ElementRef;
+
+  userMedia: MediaStream = null;
+
+  isRequestingMedia: boolean = false;
+  initializationError: boolean = false;
+
+  constructor(
+    private mediaService: UserMediaService
+  ) { }
+
+  async ngOnInit() {
+
+
+  }
+
+  async initUserMedia()
+  {
+
+    this.isRequestingMedia = true;
+    try {
+      // @ts-ignore
+      this.userMedia = await this.mediaService.getUserMedia(true, true);
+      this.videoElement.nativeElement.srcObject = this.userMedia;
+    }
+    catch (error)
+    {
+      this.initializationError = true;
+    }
+
+    this.isRequestingMedia = false;
+
+  }
+
+  async ngAfterContentInit() {
+    await this.initUserMedia();
+  }
+
+  ngOnDestroy(): void {
+    if (this.userMedia)
+    {
+      this.userMedia.getTracks().map(track => track.stop());
+    }
   }
 
 }
